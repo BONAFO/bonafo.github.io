faecc460598a4fda6404294d2f290b0e8d83209f
bonafo <bgbcnnvb@gmail.com>
2023-04-05 11:53:06 -0300

    Test1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+export default function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>GHP</h1>}/>
+          <Route path="/perro" element={<img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
