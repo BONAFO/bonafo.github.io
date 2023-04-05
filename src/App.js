2fe6959b0deb08e3ce42194794373376f694210b
bonafo <bgbcnnvb@gmail.com>
2023-04-05 12:06:24 -0300

    Test5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
     <div>
       <Router basename="/">
         <Routes>
-          <Route path="/" element={<h1>GHP</h1>}/>
+          <Route path="/" element={<h1>GHP4</h1>}/>
           <Route path="/perro" element={<img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>}/>
         </Routes>
       </Router>
