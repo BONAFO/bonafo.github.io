92b55abffb9b26c8a5f0f2f8a669d87a5289674f
bonafo <bgbcnnvb@gmail.com>
2023-04-05 12:06:10 -0300

    Test4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <Router>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<h1>GHP</h1>}/>
           <Route path="/perro" element={<img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>}/>
