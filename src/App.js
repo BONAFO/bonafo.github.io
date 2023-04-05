9a2f4967ecf8f4d6e31eb5c029bdfc73c85b87a9
bonafo <bgbcnnvb@gmail.com>
2023-04-05 12:14:12 -0300

    T5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <Router basename="/">
-        <Routes>
-          <Route path="/" element={<h1>GHP4</h1>}/>
-          <Route path="/perro" element={<img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>}/>
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        <HashRouter basename="/">
+          <Routes>
+            <Route path="/" element={<h1>GHP5</h1>} />
+            <Route
+              path="/perro"
+              element={
+                <img src="https://www.laguiadelvaron.com/wp-content/uploads/2018/03/facebook-omnitrix7-1520172526.jpg"></img>
+              }
+            />
+          </Routes>
+        </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
